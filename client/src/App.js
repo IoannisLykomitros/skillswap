7f8941cae20bd466bccc4e597afa8056a0187615
IoannisLykomitros <167572966+IoannisLykomitros@users.noreply.github.com>
2025-11-07 14:39:34 +0200

    Setup React frontend with create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SkillSwap</h1>
+      <p>Welcome to the Peer Learning Platform</p>
+    </div>
+  );
+}
+
+export default App;
